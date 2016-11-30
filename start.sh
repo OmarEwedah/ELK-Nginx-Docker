b1fa9abb485d66a8497082b6664c20188ab35f89
Omar Ewedah <omar.ewedah@gmail.com>
2016-11-30 20:09:48 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ service elasticsearch start
 
 #wait for elasticsearch to start up - https://github.com/elasticsearch/kibana/issues/3077
 counter=0
-while [ ! "$(curl localhost:9200 2> /dev/null)" -a $counter -lt 130  ]; do
+while [ ! "$(curl 172.17.0.1:9200 2> /dev/null)" -a $counter -lt 130  ]; do
   sleep 1
   ((counter++))
   echo "waiting for Elasticsearch to be up ($counter/130)"

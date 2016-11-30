798b42d1e233a138c1bc735e40deccc3a529c0aa
Omar Ewedah <omar.ewedah@gmail.com>
2016-11-30 20:16:42 +0200

    Update start.sh

--- nginx-filebeat/start.sh
+++ nginx-filebeat/start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-curl -XPUT 'http://elk:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
+curl -XPUT 'http://172.17.0.1:9200/_template/filebeat?pretty' -d@/etc/filebeat/filebeat.template.json
 service filebeat restart
-update-rc.d filebeat defaults 95 10
 
 service nginx start
 tail -f /var/log/nginx/access.log -f /var/log/nginx/error.log
